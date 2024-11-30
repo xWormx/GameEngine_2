a7eb4d0c23490c6fd019447ee150608b814348f5
xWormx <carl_johan6@hotmail.com>
2024-11-30 22:54:29 +0100

    test funcp

--- src/Main.cpp
+++ src/Main.cpp
@@ -13,24 +13,50 @@ void Sub(int a, int b)
     std::cout << a << " - " << b << " = " << a-b << "\n";
 }
 
-template <typename T> 
+
 class System
 {
     public:
         System(){}
-        
+
+        template <typename T> 
         void RegisterKeyCallBack(char key, T* object, void(T::*memFunc)())
         {
             keyCallbacks[key].emplace_back(object, memFunc);
 
         }
 
-        void ExecureCallBacks(char key)
+        template <typename T>
+        void ExecuteCallBacks(char key)
         {
+            std::vector<std::pair<T*, MemFuncPointer>>& pairVec = keyCallbacks.at(key);
+
+            for(std::pair<T*, MemFuncPointer> p : pairVec)
+            {
+                std::cout << p.first->p.second << "\n";
+            }
+
+
         }
     private:
+        template <typename T>
         typedef void (T::*MemFuncPointer)();
         std::unordered_map<char, std::vector<std::pair<T*, MemFuncPointer>>> keyCallbacks;
+};
+
+
+class Player
+{
+    public:
+        static Player* GetInstance(Vec2i pos, Vec2i sz)
+        { return new Player(pos, sz);}
+        void Move(int x, int y) { position.x += x; position.y += y; }
+
+    protected:
+        Player(Vec2i pos, Vec2i sz) : position(pos), size(sz){}
+    private:
+        Vec2i position;
+        Vec2i size;
 };
 
 #define FPS 60
@@ -43,6 +69,10 @@ int main(int argv, char **argc)
     fp1(2,3);
     FuncPointer fp2 = Sub;
     fp2(4,5);
+
+    Player* player = Player::GetInstance({0, 0}, {100, 100});
+    System sys = {};
+    sys.RegisterKeyCallback('w', &player, &Player::Move);
 
     gameEngine.Run();
 
