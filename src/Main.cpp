5f22fc30589a2ae9d843c1ed605c333c93b7f01c
xWormx <carl_johan6@hotmail.com>
2024-12-02 00:05:22 +0100

    memfunc fortsättning

--- src/Main.cpp
+++ src/Main.cpp
@@ -24,7 +24,6 @@ class System
         {
             keyCallbacks[key].emplace_back(callback);
         }
-
         void ExecuteCallBacks(char key)
         {
             for(VoidFp callback : keyCallbacks.at(key))
@@ -33,11 +32,22 @@ class System
             }
         }
     private:
-        std::unordered_map<char, std::vector<VoidFp>> keyCallbacks;        
+        std::unordered_map<char, std::vector<VoidFp>> keyCallbacks;
 };
 
-
-class Player
+class TestSprite
+{
+    public:
+        virtual void tick() = 0;
+        virtual void DoMemFunc(){}
+    protected:
+        TestSprite(Vec2i pos, Vec2i sz) : position(pos), size(sz) {} 
+    private:
+        Vec2i position, size;
+        
+};
+template <typename T>
+class Player : public TestSprite
 {
     public:
         static Player* GetInstance(Vec2i pos, Vec2i sz, std::string n)
@@ -45,18 +55,46 @@ class Player
         void Move(int x, int y) { position.x += x; position.y += y; }
         void Move() { position += acceleration; }
         void SetAcceleration(Vec2i a) { acceleration = a; }
-        void DoMemFp() { std::cout << name << " is executing: " << (obj->*memFp)() << "\n"; }
-        void SetMemFp(void (Player::*uniqueFunction)()) { memFp = uniqueFunction;}
+        void DoMemFunc() 
+        { 
+            std::cout << "\n" << name << " is executing: ";
+            (obj->*memFp)();
+            std::cout << "\n";
+        }
+        void SetMemFp(T* p, void (T::*uniqueFunction)()) { obj = p; memFp = uniqueFunction;}
         void HasName() { std::cout << " " << name << " ";}
+        void tick()
+        {
+            DoMemFunc();
+        }
     protected:
-        Player(Vec2i pos, Vec2i sz, std::string n) : position(pos), size(sz), name(n){}
+        Player(Vec2i pos, Vec2i sz, std::string n) : TestSprite(pos, sz), position(pos), size(sz), name(n){}
     private:
-        Vec2i position;
+        Vec2i position; 
         Vec2i size;
         Vec2i acceleration;
         std::string name;
-        Player* obj;        
-        void (Player::*memFp)();
+        T* obj;        
+        void (T::*memFp)();
+};
+
+class Button
+{
+    public:
+        Button(int d) : data(d) {}
+        void Print() { std::cout << "BUTTON DATA: " << data; }
+    private:
+     int data;
+};
+
+
+class Tile
+{
+    public:
+        Tile(int d) : data(d) {}
+        void Print() { std::cout << "TILE DATA: " << data; }
+    private:
+     int data;
 };
 
 
@@ -80,16 +118,20 @@ int main(int argv, char **argc)
     FuncPointer fp2 = Sub;
     fp2(4,5);
 
-    Player* player1 = Player::GetInstance({0, 0}, {100, 100}, "MasterPlayer");
-    Player* player2 = Player::GetInstance({0, 0}, {99, 9999}, "booPlayer");
-    player1->SetMemFp(&player2->HasName);
-    player2->SetMemFp(Shoot);
+    Button* b1 = new Button(12);
+    Tile* t1 = new Tile(999);
 
+    Player<Button>* player1 = Player<Button>::GetInstance({0, 0}, {100, 100}, "MasterPlayer");
+    Player<Tile>* player2 = Player<Tile>::GetInstance({0, 0}, {99, 9999}, "booPlayer");
+    player1->SetMemFp(b1, &Button::Print);
+    player2->SetMemFp(t1, &Tile::Print);
+    player1->tick();
+    player2->tick();
     System sys = {};
     //sys.RegisterKeyCallback('w', &player, &Player::Move);
-    sys.RegisterKeyCallBack('a', Move);
 
-    sys.ExecuteCallBacks('a');
+
+
     gameEngine.Run();
 
     return 0;
