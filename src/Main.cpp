4a52bd349f0c974cbe5514e41ad15c5b4cc2e850
xWormx <carl_johan6@hotmail.com>
2024-12-01 19:40:50 +0100

    memfunc

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,6 +2,7 @@
 
 
 typedef void (*FuncPointer)(int, int);  
+typedef void (*VoidFp)();
 
 void Add(int a, int b)
 {
@@ -19,45 +20,54 @@ class System
     public:
         System(){}
 
-        template <typename T> 
-        void RegisterKeyCallBack(char key, T* object, void(T::*memFunc)())
+        void RegisterKeyCallBack(char key, VoidFp callback)
         {
-            keyCallbacks[key].emplace_back(object, memFunc);
-
+            keyCallbacks[key].emplace_back(callback);
         }
 
-        template <typename T>
         void ExecuteCallBacks(char key)
         {
-            std::vector<std::pair<T*, MemFuncPointer>>& pairVec = keyCallbacks.at(key);
-
-            for(std::pair<T*, MemFuncPointer> p : pairVec)
+            for(VoidFp callback : keyCallbacks.at(key))
             {
-                std::cout << p.first->p.second << "\n";
+                callback();
             }
-
-
         }
     private:
-        template <typename T>
-        typedef void (T::*MemFuncPointer)();
-        std::unordered_map<char, std::vector<std::pair<T*, MemFuncPointer>>> keyCallbacks;
+        std::unordered_map<char, std::vector<VoidFp>> keyCallbacks;        
 };
 
 
 class Player
 {
     public:
-        static Player* GetInstance(Vec2i pos, Vec2i sz)
-        { return new Player(pos, sz);}
+        static Player* GetInstance(Vec2i pos, Vec2i sz, std::string n)
+        { return new Player(pos, sz, n);}
         void Move(int x, int y) { position.x += x; position.y += y; }
-
+        void Move() { position += acceleration; }
+        void SetAcceleration(Vec2i a) { acceleration = a; }
+        void DoMemFp() { std::cout << name << " is executing: " << (obj->*memFp)() << "\n"; }
+        void SetMemFp(void (Player::*uniqueFunction)()) { memFp = uniqueFunction;}
+        void HasName() { std::cout << " " << name << " ";}
     protected:
-        Player(Vec2i pos, Vec2i sz) : position(pos), size(sz){}
+        Player(Vec2i pos, Vec2i sz, std::string n) : position(pos), size(sz), name(n){}
     private:
         Vec2i position;
         Vec2i size;
+        Vec2i acceleration;
+        std::string name;
+        Player* obj;        
+        void (Player::*memFp)();
 };
+
+
+void Attack()
+{
+    std::cout << "Attack\n";
+}
+void Move()
+{
+    std::cout << "Move\n";
+}
 
 #define FPS 60
 
@@ -70,10 +80,16 @@ int main(int argv, char **argc)
     FuncPointer fp2 = Sub;
     fp2(4,5);
 
-    Player* player = Player::GetInstance({0, 0}, {100, 100});
-    System sys = {};
-    sys.RegisterKeyCallback('w', &player, &Player::Move);
+    Player* player1 = Player::GetInstance({0, 0}, {100, 100}, "MasterPlayer");
+    Player* player2 = Player::GetInstance({0, 0}, {99, 9999}, "booPlayer");
+    player1->SetMemFp(&player2->HasName);
+    player2->SetMemFp(Shoot);
 
+    System sys = {};
+    //sys.RegisterKeyCallback('w', &player, &Player::Move);
+    sys.RegisterKeyCallBack('a', Move);
+
+    sys.ExecuteCallBacks('a');
     gameEngine.Run();
 
     return 0;
